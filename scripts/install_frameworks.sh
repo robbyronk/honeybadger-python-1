a91a581d82008315e6c4b62f1872c39b8bc7d7a5
Dave Sullivan <dave@davesullivan.ca>
2020-05-05 08:22:29 -0700

    removed env var echos from install_frameworks script

--- scripts/install_frameworks.sh
+++ scripts/install_frameworks.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -ev
 
-echo $DJANGO_VERSION
-echo $FLASK_VERSION
-
 [ ! -z "$DJANGO_VERSION" ] && pip install Django==$DJANGO_VERSION
 [ ! -z "$FLASK_VERSION" ] && pip install Flask==$FLASK_VERSION
 echo "OK"
